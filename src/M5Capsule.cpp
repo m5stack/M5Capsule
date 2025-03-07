9a91820dc5b52951eb12ce4fe99ff2238ef594f4
sean <1026025056@qq.com>
2025-03-07 14:21:53 +0800

    fix button reference issues, use M5.getButton method

--- src/M5Capsule.cpp
+++ src/M5Capsule.cpp
@@ -1,17 +1,26 @@
+/*
+ *SPDX-FileCopyrightText: 2025 M5Stack Technology CO LTD
+ *
+ *SPDX-License-Identifier: MIT
+ */
+
 #include "M5Capsule.h"
 
 using namespace m5;
 
 M5_CAPSULE M5Capsule;
 
-void M5_CAPSULE::begin() {
+void M5_CAPSULE::begin()
+{
     M5.begin();
 }
 
-void M5_CAPSULE::begin(m5::M5Unified::config_t cfg) {
+void M5_CAPSULE::begin(m5::M5Unified::config_t cfg)
+{
     M5.begin(cfg);
 }
 
-void M5_CAPSULE::update(void) {
+void M5_CAPSULE::update(void)
+{
     M5.update();
 }
--- src/M5Capsule.h
+++ src/M5Capsule.h
@@ -1,3 +1,9 @@
+/*
+ *SPDX-FileCopyrightText: 2025 M5Stack Technology CO LTD
+ *
+ *SPDX-License-Identifier: MIT
+ */
+
 #ifndef M5CAPSULE_H
 #define M5CAPSULE_H
 
@@ -5,17 +11,17 @@
 
 namespace m5 {
 class M5_CAPSULE {
-   private:
+private:
     /* data */
 
-   public:
+public:
     void begin();
     void begin(m5::M5Unified::config_t cfg);
     Power_Class &Power     = M5.Power;
     RTC8563_Class &Rtc     = M5.Rtc;
     Speaker_Class &Speaker = M5.Speaker;
     Mic_Class &Mic         = M5.Mic;
-    Button_Class &BtnA     = M5.BtnA;
+    Button_Class &BtnA     = M5.getButton(0);
 
     /// for internal I2C device
     I2C_Class &In_I2C = m5::In_I2C;
